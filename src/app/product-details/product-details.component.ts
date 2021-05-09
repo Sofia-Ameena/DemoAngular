2df7e84942dac5ee3ac46e3bc37de303415ebf0f
Sofia <sofiaameena2002@gmail.com>
2021-05-10 03:00:59 +0530

    parent to child interaction

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProductDetailsComponent {
+  
+  @Input() prodObj:Product;
+  
 }
